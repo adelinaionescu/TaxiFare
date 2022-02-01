98d985a92e3a6efe87925b5a3a8824ca86d97308
Adelina Ionescu <ionescu.adelina1012@gmail.com>
2022-02-01 21:51:32 +0100

    Update to MLFlow

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -6,8 +6,16 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LinearRegression
+from memoized_property import memoized_property
+import mlflow
+from  mlflow.tracking import MlflowClient
+
+
 
 class Trainer():
+
+    MLFLOW_URI = "https://mlflow.lewagon.co/"
+    EXPERIMENT_NAME = "[DE] [Berlin] [adelinaionescu] TaxiFare v2"
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -48,7 +56,34 @@ class Trainer():
         pipeline = self.run()
         y_pred = pipeline.predict(X)
         rmse = compute_rmse(y_pred, y)
+        self.mlflow_create_run()
+        self.mlflow_log_metric("rmse", self.evaluate())
+        self.mlflow_log_param("model", "linear")
         return rmse
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.EXPERIMENT_NAME)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.EXPERIMENT_NAME).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+
 
 
 if __name__ == "__main__":
