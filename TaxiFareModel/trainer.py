0d751049b455eb052ba8e210ec070b5db7ef1272
Adelina Ionescu <ionescu.adelina1012@gmail.com>
2022-02-03 21:18:46 +0100

    Update scripts

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -3,14 +3,18 @@ from TaxiFareModel.utils import compute_rmse
 from TaxiFareModel.data import get_data
 from TaxiFareModel.data import clean_data
 from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn import set_config; set_config(display='diagram')
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.linear_model import LinearRegression, SGDRegressor
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression, SGDRegressor, Ridge
+from sklearn.ensemble import GradientBoostingRegressor, AdaBoostRegressor, VotingRegressor,StackingRegressor
+from xgboost import XGBRegressor
+from sklearn.svm import SVR
 from memoized_property import memoized_property
+from sklearn.metrics import make_scorer
 import mlflow
 import joblib
 from  mlflow.tracking import MlflowClient
@@ -20,7 +24,7 @@ from  mlflow.tracking import MlflowClient
 class Trainer():
 
     
-    EXPERIMENT_NAME = "[DE] [Berlin] [adelinaionescu] TaxiFare v3"
+    EXPERIMENT_NAME = "[DE] [Berlin] [adelinaionescu] TaxiFare v4"
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -33,6 +37,21 @@ class Trainer():
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
+        gboost = GradientBoostingRegressor(n_estimators=100)
+        ridge = Ridge()
+        svm = SVR(kernel='linear')
+        adaboost = AdaBoostRegressor(base_estimator=DecisionTreeRegressor(max_depth=None))
+        model = VotingRegressor(
+            estimators = [("gboost", gboost),("adaboost", adaboost),("ridge", ridge), ("svm_rbf", svm)],
+            weights = [1,1,1,1], 
+            n_jobs=-1
+        )
+        model2 = model = StackingRegressor(
+            estimators=[("gboost", gboost),("adaboost", adaboost),("ridge", ridge), ("svm_rbf", svm)],
+            final_estimator=LinearRegression(), 
+            cv=5,
+            n_jobs=-1
+        )
         dist_pipe = Pipeline([
             ('dist_trans', DistanceTransformer()),
             ('stdscaler', StandardScaler())
@@ -47,9 +66,29 @@ class Trainer():
             ], remainder="drop")
         self.pipeline = Pipeline([
             ('preproc', preproc_pipe),
-            ('linear_model', GradientBoostingRegressor(n_estimators=100))
+            ('linear_ensemble', model2)
             ])
         return self.pipeline
+
+    def set_pipeline_model(self, model):
+        dist_pipe = Pipeline([
+            ('dist_trans', DistanceTransformer()),
+            ('stdscaler', StandardScaler())
+        ])
+        time_pipe = Pipeline([
+            ('time_enc', TimeFeaturesEncoder('pickup_datetime')),
+            ('ohe', OneHotEncoder(handle_unknown='ignore'))
+        ])
+        preproc_pipe = ColumnTransformer([
+            ('distance', dist_pipe, ["pickup_latitude", "pickup_longitude", 'dropoff_latitude', 'dropoff_longitude']),
+            ('time', time_pipe, ['pickup_datetime'])
+            ], remainder="drop")
+        self.pipeline = Pipeline([
+            ('preproc', preproc_pipe),
+            ('linear_ensemble', model)
+            ])
+        return self.pipeline
+
 
     def run(self):
         """set and train the pipeline"""
@@ -62,9 +101,24 @@ class Trainer():
         pipeline = self.run()
         y_pred = pipeline.predict(X)
         rmse = compute_rmse(y_pred, y)
+        rmse_score = make_scorer(compute_rmse)
+        score = cross_val_score(pipeline, X, y, cv=5, scoring=rmse_score, n_jobs=-1).mean()
         self.mlflow_log_metric("rmse", rmse)
-        self.mlflow_log_param("model", "GBR")
-        return rmse
+        self.mlflow_log_metric("cv", score)
+        self.mlflow_log_param("model", "ensemble")
+        return (rmse,score)
+
+    def evaluate_model(self, X, y,model):
+        """evaluates the pipeline on df_test and return the RMSE"""
+        pipeline = self.run()
+        y_pred = pipeline.predict(X)
+        rmse = compute_rmse(y_pred, y)
+        rmse_score = make_scorer(compute_rmse)
+        score = cross_val_score(pipeline, X, y, cv=5, scoring=rmse_score, n_jobs=-1).mean()
+        self.mlflow_log_metric("rmse", rmse)
+        self.mlflow_log_metric("cv", score)
+        self.mlflow_log_param("model", model)
+        return (rmse,score)
 
     @memoized_property
     def mlflow_client(self):
@@ -104,6 +158,7 @@ if __name__ == "__main__":
     X = new_data.drop("fare_amount", axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
     trainer = Trainer(X_train, y_train)
+    trainer.set_pipeline()
     trainer.run()
     trainer.evaluate(X_test, y_test)
     trainer.save_model()
