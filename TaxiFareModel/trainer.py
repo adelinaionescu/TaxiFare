418ba47004235e112b7658ca87993deb044b3d79
Adelina Ionescu <ionescu.adelina1012@gmail.com>
2022-02-03 19:38:48 +0100

    Updated pipeline TaxiFare

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,21 +1,26 @@
 from TaxiFareModel.encoders import TimeFeaturesEncoder,DistanceTransformer
 from TaxiFareModel.utils import compute_rmse
+from TaxiFareModel.data import get_data
+from TaxiFareModel.data import clean_data
 from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
 from sklearn import set_config; set_config(display='diagram')
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from memoized_property import memoized_property
 import mlflow
+import joblib
 from  mlflow.tracking import MlflowClient
 
 
 
 class Trainer():
 
-    MLFLOW_URI = "https://mlflow.lewagon.co/"
-    EXPERIMENT_NAME = "[DE] [Berlin] [adelinaionescu] TaxiFare v2"
+    
+    EXPERIMENT_NAME = "[DE] [Berlin] [adelinaionescu] TaxiFare v3"
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -24,6 +29,7 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.MLFLOW_URI = "https://mlflow.lewagon.co/"
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -41,7 +47,7 @@ class Trainer():
             ], remainder="drop")
         self.pipeline = Pipeline([
             ('preproc', preproc_pipe),
-            ('linear_model', LinearRegression())
+            ('linear_model', GradientBoostingRegressor(n_estimators=100))
             ])
         return self.pipeline
 
@@ -56,14 +62,13 @@ class Trainer():
         pipeline = self.run()
         y_pred = pipeline.predict(X)
         rmse = compute_rmse(y_pred, y)
-        self.mlflow_create_run()
-        self.mlflow_log_metric("rmse", self.evaluate())
-        self.mlflow_log_param("model", "linear")
+        self.mlflow_log_metric("rmse", rmse)
+        self.mlflow_log_param("model", "GBR")
         return rmse
 
     @memoized_property
     def mlflow_client(self):
-        mlflow.set_tracking_uri(MLFLOW_URI)
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
         return MlflowClient()
 
     @memoized_property
@@ -82,15 +87,24 @@ class Trainer():
 
     def mlflow_log_metric(self, key, value):
         self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+    
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        pipeline = self.set_pipeline()
+        saved_model = joblib.dump(pipeline, "pipeline.joblib")
+        return saved_model
 
 
 
 
 if __name__ == "__main__":
-    # get data
-    # clean data
-    # set X and y
-    # hold out
-    # train
-    # evaluate
+    df = get_data()
+    new_data = clean_data(df)
+    y = new_data["fare_amount"]
+    X = new_data.drop("fare_amount", axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    trainer = Trainer(X_train, y_train)
+    trainer.run()
+    trainer.evaluate(X_test, y_test)
+    trainer.save_model()
     print('TODO')
